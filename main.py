89ccb858d63feac35224b7b9a65a7235100f0aa9
Aryan Mediratta <aryan_02@outlook.com>
2019-08-11 00:46:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import turtle
 from tqdm import tqdm
 
 shape = "FRFRF" # Initial Condition, Equilateral Triangle
-iterations = 10
+iterations = 5
+
 
 for i in range(iterations): # Iteratively build the instructions to make the Koch Snowflake
 	# Replace ____ with _/ \_
@@ -10,7 +11,7 @@ for i in range(iterations): # Iteratively build the instructions to make the Koc
 
 turtle.speed(0) # Avoid turtle moving animations
 turtle.ht() # Hide Turtle
-
+turtle.pensize(0.2)
 # Bring the turtle to an initial position such that the shape is visible on the screen
 turtle.pu()
 turtle.bk(400)
